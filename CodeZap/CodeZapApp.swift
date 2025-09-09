2c2348906fefb49d617097da90c05757ebb96eb5
erenkbr <eren.kibar97@gmail.com>
2025-09-09 23:59:33 +0300

    Initialize Firebase

--- CodeZap/CodeZapApp.swift
+++ CodeZap/CodeZapApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct CodeZapApp: App {
+    init() {
+            FirebaseApp.configure()
+        }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
